b6af7bf976dd295f1344e07548e0fbbc923d6fea
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2020-09-25 02:56:37 +0600

    Update views.py

--- HMS/Hospital/views.py
+++ HMS/Hospital/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def home_view(request):
     if request.user.is_authenticated:
-        return HttpResponseRedirect('afterlogin')
+        return HttpResponseRedirect('')
     return render(request, 'hospital/index.html')
