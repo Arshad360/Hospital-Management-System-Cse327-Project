8ed0ab547d1a2e735a524fcf5a83d376e82e5feb
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2020-09-27 00:42:10 +0600

    Update models.py

--- HMS/Hospital/models.py
+++ HMS/Hospital/models.py
@@ -14,12 +14,19 @@ departments = [('Cardiologist', 'Cardiologist'),
                ('Colon and Rectal Surgeons', 'Colon and Rectal Surgeons')
                ]
 
+# Defines of the Appointment Class
 
 class Appointment(models.Model):
-    patientId = models.PositiveIntegerField(null=True)
+    # Gets the patientId
+    patientId models.PositiveIntegerField(null=True)
+    # Gets the doctorId
     doctorId = models.PositiveIntegerField(null=True)
+    # Gets the patientName
     patientName = models.CharField(max_length=40, null=True)
+    # Gets the doctorName
     doctorName = models.CharField(max_length=40, null=True)
+    # Gets the appointmentDate
     appointmentDate = models.DateField(auto_now=True)
+    # Gets the description
     description = models.TextField(max_length=500)
     status = models.BooleanField(default=False)
