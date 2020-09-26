90aaf1f29f19cfa543eae8f46b0de1f215a30763
Rainbow <shakil.ahmed10@northsouth.edu>
2020-09-27 01:18:20 +0600

    Update

--- HMS/Hospital/models.py
+++ HMS/Hospital/models.py
@@ -1,101 +1,97 @@
 from django.db import models
 
 """"
-: All_Models
-:All classes in pascleCase
 :Title = CharField
-:Maxlength = 79
+:Maxlength = 40
 :body = Textfield
 
 """
 
-class Pharmacy(models.Model):
+    class Pharmacy(models.Model):
 
-    title = models.CharField(max_length=79)
-    pub_date = models.DateTimeField()
-    body = models.TextField()
-    images = models.ImageField(upload_to='E:\NSU\10th Semester\CSE327\Project Images')
-
+        title = models.CharField(max_length=40,null=True)
+        pub_date = models.DateTimeField()
+        body = models.TextField()
 
     class availablebloodGroup(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.CharField(max_length=40,null=True)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
 
     class bloodBank(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.CharField(max_length=40,null=True)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
 
     class coronaUpdate(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.CharField(max_length=40)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
 
     class donateBlood(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.CharField(max_length=40)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
 
     class footer(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.CharField(max_length=40)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
 
     class home(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.CharField(max_length=40)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
 
     class homeSlider(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.CharField(max_length=40)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
 
     class homeBase(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.CharField(max_length=40)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
 
     class login(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.PositiveIntegerField(max_length=40)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
 
     class navBar(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.CharField(max_length=40)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
 
     class notice(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.CharField(max_length=40)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
     class Pharmacy(models.Model):
-        title = models.CharField(max_length=79)
+        title = models.CharField(max_length=40)
         pub_date = models.DateTimeField()
         body = models.TextField()
 
 
     class saveLife(models.Model):
-       title = models.CharField(max_length=79)
+       title = models.CharField(max_length=40)
        pub_date = models.DateTimeField()
        body = models.TextField()
 
 
     class specialCare(models.Model):
-       title = models.CharField(max_length=79)
+       title = models.CharField(max_length=40, null=True)
        pub_date = models.DateTimeField()
        body = models.TextField()
 
--- HMS/Hospital/views.py
+++ HMS/Hospital/views.py
@@ -1,6 +1,8 @@
+from email.headerregistry import Group
+
+from django.contrib.admin import forms
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import Pharmacy
 
 
 # Define login function
@@ -9,95 +11,95 @@ from .models import Pharmacy
 def login(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('login')
-    return render(request, 'Template/login.html')
+    return render(request, 'template/login.html')
 
 
 # Define available_blood_group function
 # lower_case
 
-def login(request):
-    return render(request, 'Template/available_blood_group.html')
+def available_blood_group(request):
+    return render(request, 'template/available_blood_group.html')
 
 
 # Define blood_bank
 # lower_case
 
-def login(request):
-    return render(request, 'Template/blood_bank.html')
+def blood_bank(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('blood_bank')
+    return render(request, 'template/blood_bank.html')
 
 
 # Define corona_update
 # lower_case
 
-def login(request):
-    return render(request, 'Template/corona_update.html')
+def corona_update(request, user = patients, my_admin_group = doctor):
+    form = forms.corona_update()
+    if request.method == 'POST':
+        form = forms.corona_update(request.POST)
+        if form.is_valid():
+            user.save()
+            my_admin_group[0].user_set.add(user)
+            return HttpResponseRedirect('corona_update')
+    return render(request, 'hospital/corona_update.html', {'form': form})
 
 
 # Define donate_blood function
 # lower_case
 
-def login(request):
-    return render(request, 'Template/donate_blood.html')
-
+def donate_blood(request):
+    return render(request, 'template/donate_blood.html')
 
 # Define footer function
 # lower_case
 
-def login(request):
-    return render(request, 'Template/footer.html')
-
+def footer(request):
+    return render(request, 'template/footer.html')
 
 # Define home function
 # lower_case
 
-def login(request):
-    return render(request, 'Template/home.html')
-
+def home(request):
+    if request.user.is_authenticated:
+    return render(request,'template/home.html')
 
 # Define home_slider function
 # lower_case
 
-def login(request):
-    return render(request, 'Template/home_slider.html')
-
+def home_slider(request):
+    return render(request, 'template/home_slider.html')
 
 # Define home_base function
 # lower_case
 
-def login(request):
-    return render(request, 'Template/home_base.html')
-
-
-# Define navbar function
-# lower_case
-
-def login(request):
-    return render(request, 'Template/navbar.html')
-
+def home_base(request):
+    return render(request, 'template/home_base.html')
 
 # Define notice function
 # lower_case
 
-def login(request):
-    return render(request, 'Template/notice.html')
-
+def notice(request):
+    if request.user.is_authenticated:
+    return HttpResponseRedirect('notice')
+    return render(request, 'template/notice.html')
 
 # Define pharmacy function
 # lower_case
 
-def login(request):
-    return render(request, 'Template/pharmacy.html')
-
+def pharmacy(request):
+    return render(request, 'template/pharmacy.html')
 
 # Define save_life function
 # lower_case
 
-def login(request):
-    return render(request, 'Template/save_life.html')
-
+def save_life(request):
+    return render(request, 'template/save_life.html')
 
 # Define special_case function
 # lower_case
 
-def login(request):
-    return render(request, 'Template/special_case.html')
+def special_case(request):
+    return render(request, 'template/special_case.html')
+
+def about_us_view(request):
+    return render(request,'hospital/aboutus.html')
