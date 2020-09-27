e2d0030083a3c8acd77d83e378b4fec9aa06728f
Shakil Ahmed <68857266+shakilahmed80@users.noreply.github.com>
2020-09-27 17:20:04 +0600

    Update tests.py

--- HMS/Hospital/tests.py
+++ HMS/Hospital/tests.py
@@ -1,3 +1,42 @@
+from django.templatetags.i18n import language
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+
+class login(TestCase):
+
+def login(self):
+    self.user = get_user_model().objects.create_user(
+        username = 'Group6',
+        email = 'group6@gmail.com',
+        password = '123456',
+    )
+
+
+def blood_bank(self):
+    request = self.client.get(reverse("login"))
+    self.assertContains(request, 'admin')
+    self.assertEqual(request,'Access')
+    self.assertContains(request, 'blood_bank')
+    self.assertAlmostEqual(language['percent']['python'])
+    self.assertAlmostEqual(request["error"])
+    self.assertAlmostEqual(request["login"])
+
+
+def pharmacy(self):
+    request = self.client.get(reverse("pharmacy"))
+    self.assertContains(request, 'admin')
+    self.assertEqual(request,'Access')
+    self.assertContains(request, 'pharmacy')
+    self.assertAlmostEqual(language['percent']['python'])
+    self.assertAlmostEqual(request["error"])
+    self.assertAlmostEqual(request["login"])
+
+
+
+
+
+
+
+
